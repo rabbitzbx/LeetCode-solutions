c60294652afe09a059e8bf6de921ad7fd6b85cf3
zhoubingxue <zhoubingxue@qiyi.com>
2017-09-18 15:11:13 +0800

    Q32: Longest Valid Parentheses

---
+++ 32_longest-valid-parentheses.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [] 
+        maxLen = 0
+
+        for i, val in enumerate(s):
+            if val == ')' and len(stack) and s[stack[-1]] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+        stack.insert(0, -1)
+        stack.append(len(s))
+
+        for i in range(len(stack) - 1):
+            maxLen = max(maxLen, stack[i + 1] - stack[i] - 1)
+
+        return maxLen
+
+a = Solution()
+a.longestValidParentheses('()')
+a.longestValidParentheses('())')
+a.longestValidParentheses('()(()')
+a.longestValidParentheses('()(())')
+a.longestValidParentheses(')()())')

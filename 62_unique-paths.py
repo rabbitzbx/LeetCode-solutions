adc33f1e216a03c44474a69be4bc86fd15d62af3
zhoubingxue <zhoubingxue@qiyi.com>
2017-09-19 09:43:46 +0800

    Q62: unique paths

---
+++ 62_unique-paths.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        arr = []
+        for i in range(m):
+            arr.append([])
+            for j in range(n):
+                if i == 0 or j == 0:
+                    arr[i].append(1)
+                else:
+                    arr[i].append(arr[i - 1][j] + arr[i][j - 1])
+        return arr[m - 1][n - 1]
+
+a = Solution()
+a.uniquePaths(1, 2)

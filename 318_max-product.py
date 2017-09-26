c80094448b1dbb0413f6da6b4377e60ff57ca4a1
zhoubingxue <zhoubingxue@qiyi.com>
2017-09-26 16:58:49 +0800

    Q318 - Maximum Product of Word Lengths

---
+++ 318_max-product.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        maxNum = 0;
+        flags = [];
+        for i, word in enumerate(words):
+            flags.append([sum(1 << (ord(c) - 97) for c in set(word)), len(words[i])])            
+        for i in range(len(words)):
+            for j in range(i, len(words)):
+                if not flags[i][0] & flags[j][0]:
+                    maxNum = max(maxNum, flags[i][1] * flags[j][1])
+        print(maxNum)
+        return maxNum
+
+a = Solution()
+a.maxProduct(["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]);

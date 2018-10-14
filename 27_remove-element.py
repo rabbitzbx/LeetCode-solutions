cfe51471b67394a450f6dc720621dd90799509fb
rabbitzbx <zbx1209@gmail.com>
2018-10-14 17:55:59 +0800

    27. Remove Element

---
+++ 27_remove-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
+        i = 0
+
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
+
+        return i

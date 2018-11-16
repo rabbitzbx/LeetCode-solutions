fc815801471a9b616af4ecf2f7d748fb97dacfff
zhoubingxue <zhoubingxue@qiyi.com>
2018-11-16 14:59:45 +0800

    35. Search Insert Position

--- 35_search-insert-position.py
+++ 35_search-insert-position.py
@@ -6,14 +6,12 @@ class Solution:
         :rtype: int
         """
         start, end = 0, len(nums) - 1
-        result = 0
 
         while start <= end:
             mid = int((start + end) / 2)
             if target > nums[mid]:
-                result = mid + 1
                 start = mid + 1
             else:
                 end = mid - 1
 
-        return result
+        return start

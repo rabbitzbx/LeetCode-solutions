3e38bb0176f3bb860be6fd4ca70191fb61ae7a1e
zhoubingxue <zhoubingxue@qiyi.com>
2019-03-19 15:02:39 +0800

    59. Spiral Matrix II

---
+++ 59_spiral-matrix-ii.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        if n == 0:
+            return []
+
+        result = [[0] * n for _ in range(n)]
+        current = 1
+
+        for i in range(n / 2):
+            for j in range(i, n - i):
+                result[i][j] = current
+                current += 1
+            for j in range(i + 1, n - i - 1):
+                result[j][n - 1 - i] = current
+                current += 1
+            for j in reversed(range(i, n - i)):
+                result[n - 1 - i][j] = current
+                current += 1
+            for j in reversed(range(i + 1, n - i - 1)):
+                result[j][i] = current
+                current += 1
+
+        if n % 2:
+            i = n / 2
+            for j in range(i, n - i):
+                result[i][j] = current
+                current += 1
+
+        return result
+
+a = Solution()
+print(a.generateMatrix(4))
+print(a.generateMatrix(5))
+print(a.generateMatrix(6))
